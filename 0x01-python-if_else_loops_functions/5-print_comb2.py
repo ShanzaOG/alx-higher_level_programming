df46d252b91f09704cf804553af1b87b88a06376
ShanzaOG <allanshanza@gmail.com>
2023-05-09 09:27:21 -0700

    corrections

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i in range(100):
-    print('{:02d}'.format(i), end='')
-    if i != 99:
-        print(', ', end = '')
-    else:
-        print()
+for i in range(99):
+    print('{:02d}'.format(i), end=', ')
+print(i + 1)
