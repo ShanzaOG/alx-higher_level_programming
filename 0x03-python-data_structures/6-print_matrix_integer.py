f00541906a2c15de686a13185d4cdab680b98219
ShanzaOG <allanshanza@gmail.com>
2023-05-14 06:01:39 -0700

    printing marix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,12 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     """
     Prints a matrix of integers.
-
-    Parameters:
-    matrix (list of list of int): The input matrix.
-
-    Returns:
-    None
     """
     for row in matrix:
         for i, num in enumerate(row):
