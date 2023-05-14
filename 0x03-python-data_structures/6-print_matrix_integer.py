ee62be9585f472082d0644ea05124d910379a293
ShanzaOG <allanshanza@gmail.com>
2023-05-14 05:44:50 -0700

    printing marix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -12,6 +12,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, num in enumerate(row):
             if i < len(row) - 1:
-                print("{:3d}".format(num), end=" ")
+                print("{:d}".format(num), end=" ")
             else:
-                print("{:3d}".format(num))
+                print("{:d}".format(num))
