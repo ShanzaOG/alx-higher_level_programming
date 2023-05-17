b063d86744c049703ee2af63b9ddcc399623df29
ShanzaOG <allanshanza@gmail.com>
2023-05-17 10:51:08 -0700

    correction

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,12 +3,4 @@ def search_replace(my_list, search, replace):
     """
     Function to search and replace an element
     """
-    new_list = []
-
-    for element in my_list:
-        if element == search:
-            new_list.append(replace)
-        else:
-            new_list.append(element)
-
-    return new_list
+    return ([elem if elem != search else replace for elem in my_list])
