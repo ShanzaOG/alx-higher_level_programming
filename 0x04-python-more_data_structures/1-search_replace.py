312a4055eec728332800ccd3932cfb75231dba21
ShanzaOG <allanshanza@gmail.com>
2023-05-17 10:53:53 -0700

    correction

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,8 @@
-#!/us/bin/python3
+#!/usr/bin/python3
+
+
 def search_replace(my_list, search, replace):
     """
-    Function to search and replace an element
+    Search and replace
     """
     return ([elem if elem != search else replace for elem in my_list])
