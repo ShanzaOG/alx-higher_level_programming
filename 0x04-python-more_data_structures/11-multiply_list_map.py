db10444a6cf3ecf0489d59e029e7ddfd127216c8
ShanzaOG <allanshanza@gmail.com>
2023-05-17 14:11:50 -0700

    corrections

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-
 def mutiply_list_map(my_list=[], number=0):
     """
     multiply without using any loops
     """
-    return (list(map(lambda x: x*number, my_list)))
+    return (list(map(lambda x: x * number, my_list)))
