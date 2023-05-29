40296fd7d79376ced9a8ee1e335771cc707ace02
ShanzaOG <allanshanza@gmail.com>
2023-05-29 11:43:22 -0700

    corrections

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@
 
 def safe_print_list(my_list=[], x=0):
     """
-    prints a list of anything, but only prints the integers
+    prints list of anything, only prints the integers
     Returns the amount of integers printed
     """
     printed = 0
