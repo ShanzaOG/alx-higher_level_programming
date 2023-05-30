d02a2af86e4dbf7d01cecc67ce9890424a550b4b
ShanzaOG <allanshanza@gmail.com>
2023-05-30 12:34:22 -0700

    class square with private attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -16,4 +16,8 @@ class Square:
         Args:
             size: A private instance size
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
