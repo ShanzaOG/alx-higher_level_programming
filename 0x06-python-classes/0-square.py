a9bc0b76e18520af97ec2b19c2e96adfe8ffcea8
ShanzaOG <allanshanza@gmail.com>
2023-05-30 12:15:42 -0700

    initial commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 30 22:11:54 2023
+@author: ShanzaOG
+"""
+
+
+class Square:
+    """Empty class Square that defines a square
+
+    Attributes:
+        empty
+    """
+    pass
